8dbab27b51446db46a43c6d72b59713d8e69e577
Andres Navarro <andresnavag93@gmail.com>
2022-01-14 20:59:53 -0400

    :hammer: Finish player movement and some VFX

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     public Joystick joystick;
-    public float speed = 4;
+    public float speed = 2f;
     private Vector3 velocityVector = Vector3.zero; // Initial Velocity
 
     public float maxVelocityChange = 4f;
     private Rigidbody rb;
+
+    public float tiltAmount = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +35,8 @@ public class MovementController : MonoBehaviour
 
         //Apply Movement
         Move(_movementVelocityVector);
+
+        transform.rotation = Quaternion.Euler(joystick.Vertical * speed * tiltAmount, 0, -1 * joystick.Horizontal * speed * tiltAmount);
     }
 
     void Move(Vector3 movementVelocityVector)
@@ -42,7 +46,7 @@ public class MovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if ( velocityVector != Vector3.zero)
+        if (velocityVector != Vector3.zero)
         {
             // Get rigifbod's currten velocity
             Vector3 velocity = rb.velocity;
